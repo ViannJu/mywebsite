9325e93817318e2fc9b97f49c82ed2418c33cc1b
Jorge Ambrocio Ventura <53419722+JorgeAmbrocio@users.noreply.github.com>
2021-06-11 16:03:01 -0600

    Cambios para export

--- myapp/src/Routes.js
+++ myapp/src/Routes.js
@@ -9,10 +9,10 @@ const Routes = () => {
     return (
         <Switch>
             <Route exact path = "/" component={Navigation}/>
-            <Route exact path = "/mywebsite" component={Navigation}/>
-            <Route exact path = "/mywebsite/reporte" component={Reporte}/>
-            <Route exact path = "/mywebsite/reporteTabla" component={TablaSimbolos}/>
-            <Route exact path = "/mywebsite/reporteErrores" component={TablaErrores}/>
+            <Route exact path = "/tytusx/20211SVAC/G17" component={Navigation}/>
+            <Route exact path = "/tytusx/20211SVAC/G17/reporte" component={Reporte}/>
+            <Route exact path = "/tytusx/20211SVAC/G17/reporteTabla" component={TablaSimbolos}/>
+            <Route exact path = "/tytusx/20211SVAC/G17/reporteErrores" component={TablaErrores}/>
         </Switch>
     );
 }
